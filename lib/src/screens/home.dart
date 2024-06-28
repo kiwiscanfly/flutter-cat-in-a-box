0376109f9cc77e38f7a65dcc5bbe573f153a3eb9
Rebecca Milne <rebecca@bex.codes>
2024-06-28 13:50:29 +1200

    Box animation and readme

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/cat.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,6 +12,8 @@ class Home extends StatefulWidget {
 class HomeState extends State<Home> with TickerProviderStateMixin {
   late Animation<double> catAnimation;
   late AnimationController catController;
+  late Animation<double> boxAnimation;
+  late AnimationController boxController; 
   Color colorBrown = const Color.fromARGB(255, 166, 115, 50);
 
   @override
@@ -28,13 +31,38 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
         curve: Curves.easeIn,
       ),
     );
+
+    boxController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    boxAnimation = Tween(begin: pi * 0.6, end: pi * 0.65).animate(
+      CurvedAnimation(
+        parent: boxController,
+        curve: Curves.linear,
+
+      ),
+    );
+
+    boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      }
+      else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+    boxController.forward();
   }
 
   onTap() {
     if (catController.status == AnimationStatus.completed) {
       catController.reverse();
+      boxController.forward();
     } else {
       catController.forward();
+      boxController.stop();
     }
   }
 
@@ -51,6 +79,8 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
             children: [
               buildCatAnimation(),
               buildBox(),
+              buildLeftFlap(),
+              buildRightFlap(),
             ],
           ),
         ),
@@ -78,6 +108,48 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       color: colorBrown,
       width: 200.0,
       height: 200.0,
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      left: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation, 
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: boxAnimation.value,
+            alignment: Alignment.topLeft,
+            child: child,
+          );
+        },
+        child: Container(
+          color: colorBrown,
+          width: 100.0,
+          height: 10.0,
+        )
+      ),
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      right: 3.0,
+      child: AnimatedBuilder(
+        animation: boxAnimation, 
+        builder: (context, child) {
+          return Transform.rotate(
+            angle: boxAnimation.value * -1,
+            alignment: Alignment.topRight,
+            child: child,
+          );
+        },
+        child: Container(
+          color: colorBrown,
+          width: 100.0,
+          height: 10.0,
+        )
+      ),
     );
   }
 }
